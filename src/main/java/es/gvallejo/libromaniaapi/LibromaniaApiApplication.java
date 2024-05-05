273f1fa4b28f6b566425864d91296a0cf5dc9f7d
Gabriel Vallejo <gabi.vallejo44@gmail.com>
2024-05-05 13:05:44 +0200

    JWT Añadido

---
+++ src/main/java/es/gvallejo/libromaniaapi/LibromaniaApiApplication.java
@@ -0,0 +1,13 @@
+package es.gvallejo.libromaniaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibromaniaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibromaniaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/gvallejo/libromaniaapi/LibromaniaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.gvallejo.libromaniaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibromaniaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
